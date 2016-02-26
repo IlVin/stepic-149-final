357e74b8279b660d7044c3f3a0319558d7afa478
Ilia Vinokurov <ilvin@mail.ru>
2016-02-26 11:50:30 +0300

    log off

--- src/cgi.cpp
+++ src/cgi.cpp
@@ -12,18 +12,18 @@ std::string t200 = "HTTP/1.0 200 OK\r\nContent-length: %d\r\nConnection: close\r
 std::string t404 = "HTTP/1.0 404 NOT FOUND\r\nContent-Type: text/html\r\nContent-length: 9\r\nConnection: close\r\n\r\nNOT_FOUND";
 
 void TCGI::start(TBuffer * rb, TBuffer * wb) {
-    std::ofstream log("/tmp/log.txt", std::ios::out | std::ios::app);
-    log << "REQ: [" << rb->asString() << "]" << std::endl;
+//    std::ofstream log("/tmp/log.txt", std::ios::out | std::ios::app);
+//    log << "REQ: [" << rb->asString() << "]" << std::endl;
     TRequest * r = new TRequest(rb);
     if (r->status < 0) {
-        log << "RESP: [" << tbad << "]" << std::endl;
+//        log << "RESP: [" << tbad << "]" << std::endl;
         wb->append(tbad);
     } else {
         std::string fpath = folder + r->path;
         struct stat st;
         int r = stat(fpath.c_str(), &st);
         if (r == -1 || !(st.st_mode & S_IFREG)) {
-            log << "RESP: [" << t404 << "]" << std::endl;
+//            log << "RESP: [" << t404 << "]" << std::endl;
             wb->append(t404);
         } else {
             std::ifstream * f = new std::ifstream(fpath, std::ios::in );
@@ -33,7 +33,7 @@ void TCGI::start(TBuffer * rb, TBuffer * wb) {
             std::stringstream ss;
             ss  << "HTTP/1.0 200 OK\r\nContent-length: " << content.size()
                 << "\r\nConnection: close\r\nContent-Type: text/html\r\n\r\n" << content;
-            log << "RESP: [" << ss.str() << "]" << std::endl;
+//            log << "RESP: [" << ss.str() << "]" << std::endl;
             wb->append(ss.str());
             delete f;
         }
